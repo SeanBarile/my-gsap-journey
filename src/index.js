51dd9458467d23b3cbbdde28042802d183b3fec6
Sean Barile <48389168+SeanBarile@users.noreply.github.com>
2025-10-09 15:30:04 -0700

    Add: GSAP grid component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { Clock } from "./clock";
-import { Marquee } from "./marquee";
+import { Clock } from './clock';
+import { Marquee } from './marquee';
+import { Grid } from './grid';
 
-import { reveal } from "./helpers/reveal";
+import { reveal } from './helpers/reveal';
 
-const clock = new Clock(".hero_clock");
+const clock = new Clock('.hero_clock');
 
-const marqueeElements = document.querySelectorAll(".marquee");
+const marqueeElements = document.querySelectorAll('.marquee');
 marqueeElements.forEach(
   (marqueeElement, index) => new Marquee(marqueeElement, index + 1 * 20)
 );
+const grid = new Grid();
 reveal();
